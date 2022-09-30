2ded403cd3ddd129120a0fb087e63629287cb411
ralph <rlv@DESKTOP-H7PS53L>
2022-09-30 11:28:25 -0700

    api doc progress

--- Docs/js/docviewer.js
+++ Docs/js/docviewer.js
@@ -24,6 +24,8 @@
 
             // Go through each method "post", "get", etc.
             for(let methodName of methodNames) {
+                const methodObj = endpoints.paths[pathName][methodName];
+
                 // Create an API box
                 const apiBox = document.createElement('div');
                 apiBox.classList.add('box', methodName);
@@ -42,7 +44,45 @@
                 epUrlEl.textContent = pathName;
                 epContainerEl.appendChild(epUrlEl);
 
+                // Create extended explanation box
+                const explEl = document.createElement('div');
+                explEl.classList.add('explanation');
+                explEl.innerHTML = `
+                    <div class="header">Parameters</div>
+                    <div class="params"></div>
+                `;
+
+                if(methodObj['parameters'] != null) {
+                    // Add parameters
+                    for(let param of methodObj['parameters']) {
+                        const paramContainer = document.createElement('div');
+                        paramContainer.classList.add('parameter');
+
+                        const typeEl = document.createElement('span');
+                        typeEl.classList.add('type');
+                        typeEl.textContent = param['in'];
+
+                        const schemaEl = document.createElement('span');
+                        schemaEl.classList.add('schema');
+                        schemaEl.textContent = param.schema.type;
+
+                        const nameEl = document.createElement('span');
+                        nameEl.classList.add('name');
+                        nameEl.textContent = (param['in'] == "path") ? `{${param['name']}}` : param['name'];
+
+                        // Append finished elements
+                        paramContainer.appendChild(typeEl);
+                        paramContainer.appendChild(schemaEl);
+                        paramContainer.appendChild(nameEl);
+
+                        explEl.querySelector(".params").appendChild(paramContainer);
+                    }
+                } else
+                    explEl.querySelector(".params").appendChild(new Text("(no parameters)"));
+
+                // Add constructed containers in order
                 apiBox.appendChild(epContainerEl);
+                apiBox.appendChild(explEl);
 
                 // Add API box to document
                 apiViewCt.appendChild(apiBox);
