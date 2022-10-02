7730bd04054425922ec59cc867133448ce82d2b5
code-syl <ejjvandelaar@gmail.com>
2022-10-02 12:57:10 +0200

    Added Response object

---
+++ order-api/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace order_api;
+
+public class Response<TData> where TData : class?
+{
+    
+    public TData? Data { get; init; }
+    public bool Successful { get; init; } = true;
+    public string Message { get; init; } = string.Empty;
+
+}
